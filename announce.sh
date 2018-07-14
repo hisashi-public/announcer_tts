82274dca30afc2772e703f65860a17439d8a2272
master <username@example.com>
2018-07-15 05:04:01 +0900

    ..

--- announce.sh
+++ announce.sh
@@ -6,7 +6,7 @@ cat ${1} | while read line ; do
     base=$(printf "%06d" ${count})
     echo "${line}" > ${base}.txt
 #    ./voicetext.py ${base}.txt ${base}.mp3 || exit -1
-    ./voicetext.py "${base}.txt" "${base}.mp3" &
+    ./voicetext.py "${base}.txt" "${base}.mp3" ${2} &
     echo -n " ${base}.mp3" >> list.txt
     count=$(echo "${count} + 1" | bc)
 done
@@ -19,10 +19,14 @@ for f in $(cat list.txt); do
     done
 done
 cat list.txt
-sox $(cat list.txt) output.wav || exit -1
+sox -r 44100 -n zero.wav trim 0 0.5 || exit -1
+LIST=""
+for f in $(cat list.txt); do
+    LIST="${LIST} zero.wav ${f} "
+done
+sox ${LIST} zero.wav -r 44100 output.wav
 rm list.txt
 rm 000???.??? 
-sox output.wav echoed.wav echo 1.0 0.75 100 0.5 reverb || exit -1
+sox output.wav ${3} echo 1.0 0.75 100 0.5 reverb || exit -1
 rm output.wav
-play echoed.wav
 
